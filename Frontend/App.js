02befb7be8948388d71bf3ab6b25704cdb0f846b
Natália Ramalho <NataliaRamalho@users.noreply.github.com>
2020-11-14 17:19:22 -0300

    Update

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App';
 
 export default function App() {
   return (
     <View style={styles.container}>
-     
+      <Text> OLA MUNDO</Text>
       <StatusBar style="auto" />
     </View>
   );
